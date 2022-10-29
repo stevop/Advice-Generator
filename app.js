c1784f64759c2675da67a722285ce493c4c8ed5a
StefanPacola <stevo.pacola@gmail.com>
2022-10-29 14:20:03 +0200

    finished mobile design

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 const dice = document.querySelector(".dice");
+const diceBg = document.querySelector("svg");
 const p = document.querySelector("#advice-text");
+const h1 = document.querySelector("#advice-id");
 
 // FETCHING DATA
 const getAdvice = async () => {
     let res = await axios.get("https://api.adviceslip.com/advice");
-    return res.data.slip.advice;
+    return res.data.slip;
 }
 
  // CLICK TO DICE
- dice.addEventListener("click", async function(){
+dice.addEventListener("click", async function(){
     let adviceGetter = await getAdvice();
+    h1.innerHTML = "";
+    h1.textContent = `"Advice ${adviceGetter.id}"`;
+
     p.innerHTML = "";
-    p.textContent = `"${adviceGetter}"`;
+    p.textContent = `"${adviceGetter.advice}"`;
  });
+
+// TREBALO BY PREROBIT
+diceBg.addEventListener("click", async function(){
+    let adviceGetter = await getAdvice();
+    h1.innerHTML = "";
+    h1.textContent = `"Advice ${adviceGetter.id}"`;
+
+    p.innerHTML = "";
+    p.textContent = `"${adviceGetter.advice}"`;
+});
