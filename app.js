5f11b17db0ae240bd5bf49dbfe5a15f345f68f55
StefanPacola <stevo.pacola@gmail.com>
2022-11-10 11:45:37 +0100

    changed formatting

--- app.js
+++ app.js
@@ -5,26 +5,26 @@ const h1 = document.querySelector("#advice-id");
 
 // FETCHING DATA
 const getAdvice = async () => {
-    let res = await axios.get("https://api.adviceslip.com/advice");
-    return res.data.slip;
-}
+	let res = await axios.get("https://api.adviceslip.com/advice");
+	return res.data.slip;
+};
 
- // CLICK TO DICE
-dice.addEventListener("click", async function(){
-    let adviceGetter = await getAdvice();
-    h1.innerHTML = "";
-    h1.textContent = `"Advice ${adviceGetter.id}"`;
+// CLICK TO DICE
+dice.addEventListener("click", async function () {
+	let adviceGetter = await getAdvice();
+	h1.innerHTML = "";
+	h1.textContent = `"Advice ${adviceGetter.id}"`;
 
-    p.innerHTML = "";
-    p.textContent = `"${adviceGetter.advice}"`;
- });
+	p.innerHTML = "";
+	p.textContent = `"${adviceGetter.advice}"`;
+});
 
 // TREBALO BY PREROBIT
-diceBg.addEventListener("click", async function(){
-    let adviceGetter = await getAdvice();
-    h1.innerHTML = "";
-    h1.textContent = `"Advice ${adviceGetter.id}"`;
+diceBg.addEventListener("click", async function () {
+	let adviceGetter = await getAdvice();
+	h1.innerHTML = "";
+	h1.textContent = `"Advice ${adviceGetter.id}"`;
 
-    p.innerHTML = "";
-    p.textContent = `"${adviceGetter.advice}"`;
+	p.innerHTML = "";
+	p.textContent = `"${adviceGetter.advice}"`;
 });
